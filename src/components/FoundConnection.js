2b4bf2c607262f744176ebc2528889f87837e081
Damian Lupa <damian.lupa88@gmail.com>
2019-08-21 20:13:31 +0200

    worked with input component

--- src/components/FoundConnection.js
+++ src/components/FoundConnection.js
@@ -45,6 +45,9 @@ class FoundConnection extends Component {
         ]
 
         let test = table22.slice(0, connectionLength)
+        console.log(test)
+        console.log(connectionLength)
+        console.log("pobrane dane", connection)
 
 
         let table = [
@@ -52,20 +55,14 @@ class FoundConnection extends Component {
 
             [2, `${connection[2].from_point_name}`, `${connection[2].to_point_name}`, `${connection[2].mode}`, `${connection[2].duration}`, `${connection[2].mode === "foot" ? "-" : this.showPrice(connection[2].mode, connection[2].duration)}`],
 
-            [3, `${connection[3].from_point_name}`, `${connection[3].to_point_name}`, `${connection[3].mode}`, `${connection[3].duration}`, `${connection[3].mode === "foot" ? "-" : this.showPrice(connection[3].mode, connection[3].duration)}`],
+            // [3, `${connection[3].from_point_name}`, `${connection[3].to_point_name}`, `${connection[3].mode}`, `${connection[3].duration}`, `${connection[3].mode === "foot" ? "-" : this.showPrice(connection[3].mode, connection[3].duration)}`],
 
-            [4, `${connection[4].from_point_name}`, `${connection[4].to_point_name}`, `${connection[4].mode}`, `${connection[4].duration}`, `${connection[4].mode === "foot" ? "-" : this.showPrice(connection[4].mode, connection[4].duration)}`],
+            // [4, `${connection[4].from_point_name}`, `${connection[4].to_point_name}`, `${connection[4].mode}`, `${connection[4].duration}`, `${connection[4].mode === "foot" ? "-" : this.showPrice(connection[4].mode, connection[4].duration)}`],
 
-            [5, `${connection[5].from_point_name}`, `${connection[5].to_point_name}`, `${connection[5].mode}`, `${connection[5].duration}`, `${connection[5].mode === "foot" ? "-" : this.showPrice(connection[5].mode, connection[5].duration)}`],
+            // [5, `${connection[5].from_point_name}`, `${connection[5].to_point_name}`, `${connection[5].mode}`, `${connection[5].duration}`, `${connection[5].mode === "foot" ? "-" : this.showPrice(connection[5].mode, connection[5].duration)}`],
 
-            [6, `${connection[6].from_point_name}`, `${connection[6].to_point_name}`, `${connection[6].mode}`, `${connection[6].duration}`, `${connection[6].mode === "foot" ? "-" : this.showPrice(connection[6].mode, connection[6].duration)}`],
+            // [6, `${connection[6].from_point_name}`, `${connection[6].to_point_name}`, `${connection[6].mode}`, `${connection[6].duration}`, `${connection[6].mode === "foot" ? "-" : this.showPrice(connection[6].mode, connection[6].duration)}`],
         ]
-
-
-
-
-        // console.log(test)
-        // console.log(connectionLength)
 
 
         const row = table.map((table) =>
@@ -79,11 +76,8 @@ class FoundConnection extends Component {
             </tr>
         )
 
-
-
         return (
             <>
-
                 <table className="table table-hover text-center mt-5">
                     <thead>
                         <tr>
@@ -93,7 +87,6 @@ class FoundConnection extends Component {
                             <th scope="col">Connection</th>
                             <th scope="col">Travel time</th>
                             <th scope="col">Normal price</th>
-
                         </tr>
                     </thead>
                     <tbody>
@@ -101,70 +94,6 @@ class FoundConnection extends Component {
                     </tbody>
                 </table>
 
-
-
-                {/* <div className="row mt-5">
-                    <div className="col-3 text-center">
-
-                        <h5>From</h5>
-
-                        <p>{connection[1].from_point_name}</p>
-                        <p>{connection[2].from_point_name}</p>
-                        <p>{connection[3].from_point_name}</p>
-                        <p>{connection[4].from_point_name}</p>
-                        <p>{connection[5].from_point_name}</p>
-                        <p>{connection[6].from_point_name}</p>
-
-                    </div>
-
-                    <div className="col-3 text-center">
-
-                        <h5>To</h5>
-
-                        <p>{connection[1].to_point_name}</p>
-                        <p>{connection[2].to_point_name}</p>
-                        <p>{connection[3].to_point_name}</p>
-                        <p>{connection[4].to_point_name}</p>
-                        <p>{connection[5].to_point_name}</p>
-                        <p>{connection[6].to_point_name}</p>
-
-                    </div>
-
-                    <div className="col text-center">
-                        <h5>Connection</h5>
-                        <p>{connection[1].mode}{connection[1].mode === "bus" ? ` (line: ${connection[1].line_name})` : null}</p>
-                        <p>{connection[2].mode}{connection[2].mode === "bus" ? ` (line: ${connection[2].line_name})` : null}</p>
-                        <p>{connection[3].mode}{connection[3].mode === "bus" ? ` (line: ${connection[3].line_name})` : null}</p>
-                        <p>{connection[4].mode}{connection[4].mode === "bus" ? ` (line: ${connection[4].line_name})` : null}</p>
-                        <p>{connection[5].mode}{connection[5].mode === "bus" ? ` (line: ${connection[5].line_name})` : null}</p>
-                        <p>{connection[6].mode}{connection[6].mode === "bus" ? ` (line: ${connection[6].line_name})` : null}</p>
-
-
-                    </div>
-
-                    <div className="col text-center">
-                        <h5>Travel time</h5>
-                        <p>{connection[1].duration}</p>
-                        <p>{connection[2].duration}</p>
-                        <p>{connection[3].duration}</p>
-                        <p>{connection[4].duration}</p>
-                        <p>{connection[5].duration}</p>
-                        <p>{connection[6].duration}</p>
-
-
-                    </div>
-
-                    <div className="col text-center">
-                        <h5>Normal price</h5>
-                        <p>{connection[1].mode === "foot" ? "-" : this.showPrice(connection[1].mode, connection[1].duration)}</p>
-                        <p>{connection[2].mode === "foot" ? "-" : this.showPrice(connection[2].mode, connection[2].duration)}</p>
-                        <p>{connection[3].mode === "foot" ? "-" : this.showPrice(connection[3].mode, connection[3].duration)}</p>
-                        <p>{connection[4].mode === "foot" ? "-" : this.showPrice(connection[4].mode, connection[4].duration)}</p>
-                        <p>{connection[5].mode === "foot" ? "-" : this.showPrice(connection[5].mode, connection[5].duration)}</p>
-                        <p>{connection[6].mode === "foot" ? "-" : this.showPrice(connection[6].mode, connection[6].duration)}</p>
-
-                    </div>
-                </div> */}
             </>
         );
     }
